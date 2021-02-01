96d5e9178efb9d0e87b4446c8c9d77ea2d086468
admin <admin@admins-MBP.attlocal.net>
2021-02-01 10:48:15 -0600

    update 1

---
+++ index.js
@@ -0,0 +1,31 @@
+const rivers = [
+    {name: "Red", length: 225, depth: 34, deathsThisYear: true},
+    {name: "Sabine", length: 233, depth: 22, deathsThisYear: true},
+    {name: "Angelina", length: 111, depth: 33, deathsThisYear: false},
+    {name: "Brazos", length: 245, depth: 14, deathsThisYear: true},
+    {name: "Trinity", length: 229, depth: 11, deathsThisYear: false},
+    {name: "Neches", length: 333, depth: 29, deathsThisYear: false},
+    {name: "Rio Grande", length: 255, depth: 28, deathsThisYear: true},
+    {name: "San Marcos", length: 333, depth: 30, deathsThisYear: true},
+    {name: "Guadalupe", length: 277, depth: 20, deathsThisYear: true},
+    {name: "Pedernales", length: 266, depth: 12, deathsThisYear: true},
+    {name: "Colorado", length: 302, depth: 19, deathsThisYear: false},
+    {name: "Blanco", length: 277, depth: 24, deathsThisYear: true},
+    {name: "Pecos", length: 309, depth: 27, deathsThisYear: false},
+    {name: "Lavaca", length: 312, depth: 10, deathsThisYear: false},
+    {name: "Llano", length: 334, depth: 18, deathsThisYear: true},
+    {name: "Nueces", length: 290, depth: 17, deathsThisYear: false},
+    {name: "Devils", length: 228, depth: 29, deathsThisYear: true},
+    {name: "San Antonio", length: 128, depth: 31, deathsThisYear: true},
+    {name: "San Gabriel", length: 102, depth: 32, deathsThisYear: false},
+    {name: "San Jacinto", length: 144, depth: 13, deathsThisYear: true},
+    {name: "San Saba", length: 112, depth: 23, deathsThisYear: false},
+    {name: "Wichita", length: 101, depth: 9, deathsThisYear: false},
+    {name: "Frio", length: 137, depth: 8, deathsThisYear: true},
+    {name: "Navidad", length: 151, depth: 7, deathsThisYear: true},
+    {name: "Lampasas", length: 188, depth: 6, deathsThisYear: false}
+]
+
+console.log(rivers)
+
+
